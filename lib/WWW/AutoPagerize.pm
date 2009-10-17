0deab0ffde65b4cacba10e7bd193375fcacacbde
motemen <motemen@gmail.com>
2009-10-18 01:18:02 +0900

    initial commit

---
+++ lib/WWW/AutoPagerize.pm
@@ -0,0 +1,33 @@
+package WWW::AutoPagerize;
+
+use strict;
+use warnings;
+our $VERSION = '0.01';
+
+1;
+__END__
+
+=head1 NAME
+
+WWW::AutoPagerize -
+
+=head1 SYNOPSIS
+
+  use WWW::AutoPagerize;
+
+=head1 DESCRIPTION
+
+WWW::AutoPagerize is
+
+=head1 AUTHOR
+
+motemen E<lt>motemen@gmail.comE<gt>
+
+=head1 SEE ALSO
+
+=head1 LICENSE
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=cut
